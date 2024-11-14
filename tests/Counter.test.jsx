d8189f8c59a500f2acb660872793ee2d2ef26660
Laura Gayo <47537550+lauragayo@users.noreply.github.com>
2024-11-14 19:06:30 -0300

    Update Counter.test.jsx

--- tests/Counter.test.jsx
+++ tests/Counter.test.jsx
@@ -1,16 +1,1 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import Counter from "../src/components/Counter";
 
-describe("<Counter />", () => {
-  test("renders the Counter component", () => {
-    render(<Counter />);
-    const countElement = screen.getByText(/Count: 0/i);
-    const buttonElement = screen.getByText(/Increment/i);
-    
-    expect(countElement).toBeInTheDocument();
-    expect(buttonElement).toBeInTheDocument();
-
-    fireEvent.click(buttonElement);
-    expect(screen.getByText(/Count: 1/i)).toBeInTheDocument();
-  });
-});
